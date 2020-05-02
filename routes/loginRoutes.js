1a4cc8f5bc373e692b1c3082e2e70c88d71b1b8c
boyandonchev <boyandon@gmail.com>
2020-05-03 01:38:27 +0300

    get user ip in feedback

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -4,21 +4,26 @@ const db = require('../db/mongoDb');
 module.exports = function(app){
 
     app.get('/',(req, res) => {
-        console.log( req.ip );
+        console.log( req.ip + ' -> /');
+
         return res.redirect('/login');
 
     })
 
     app.get('/login', (req, res) => {
+        console.log( req.ip + ' -> /login');
+
         res.render('login',  {al: false})
     })
 
     app.get('/signup', (req, res) => {
+        console.log( req.ip + ' -> /signup');
+
         res.render('signup', {al: false})
     })
 
     app.post('/login', (req, res) => {
-        console.log('Someone is loging in');
+        console.log(req.ip + ' -> loging in');
 
         var username = req.body.username;
         var password = req.body.password;
@@ -30,25 +35,25 @@ module.exports = function(app){
 
                         res.cookie('username', username, { maxAge: 900000 });
 
-                        console.log(username + " just logged in");
+                        console.log(req.ip + " -> " + username + " just logged in");
                         return res.redirect("/chat")
                     } else {
-                        console.log(username + " entered wrong password -> " + password);
+                        console.log(req.ip + " -> " + username + " entered wrong password => " + password);
                         res.render('login', { al: true, msg: "Wrong username or password." })
                     }
                 } else {
-                    console.log("username " + username + " does not exist");
+                    console.log(req.ip + " -> " + "username " + username + " does not exist");
                     res.render('login', { al: true, msg: "Wrong username or password." })
                 }
             })
         } else {
-            console.log(username + " entered blank password");
+            console.log(req.ip + " -> " + username + " entered blank password");
             res.render('login', { msg: "Password can not be blank.", al: true })
         }
     })
 
     app.post('/signup', async (req, res) => {
-        console.log('Someone is signing up');
+        console.log(req.ip + " -> " + ' signing up');
 
         var username = req.body.username;
         var password = req.body.password;
@@ -62,19 +67,19 @@ module.exports = function(app){
                 if (password !== "") {
                     if (password === confirmPassword) {
                         db.insertUser(username, hash, (dbResponse) => {
-                            console.log("Account " + username + " created");
+                            console.log(req.ip + " -> " + "Account " + username + " created");
                             res.render('login', { msg: "Account " + username + " created. Now you can login.", al: true })
                         })
                     }else {
-                        console.log("Wrong confirm password -> " + password + " != " + confirmPassword);
+                        console.log(req.ip + " -> " + "Wrong confirm password => " + password + " != " + confirmPassword);
                         res.render('signup', { msg: "You did not confirm the password correctly.", al: true })
                     }
                 }else {
-                    console.log("Blank password");
+                    console.log(req.ip + " -> " + "Blank password");
                     res.render('signup', { msg: "Password can not be blank.", al: true })
                 }
             }else {
-                console.log("Username " + username + " exists");
+                console.log(req.ip + " -> " + "Username " + username + " exists");
                 res.render('signup', { msg: "Username " + username + " already exists.", al: true })
             }
         })
