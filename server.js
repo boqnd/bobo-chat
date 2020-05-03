eae973ff87af0d222db1c99e5df2f3024595597a
boyandonchev <boyandon@gmail.com>
2020-05-03 03:03:24 +0300

    3:00am changes

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('trust proxy', true)
 
@@ -20,6 +21,19 @@ io.on('connection', (socket) => {
     })
 });
 
+app.post('/selectUser', (req, res) => {
+  let username= req.body.username;
+  res.send (username);
+});
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('username');
+
+    res.render('login',  {al: false})
+})
+
+
+//<form action="/selectUser" method='get' id="selectUserForm">
 http.listen(4002, () => {
     console.log('Server is running!')
 });
